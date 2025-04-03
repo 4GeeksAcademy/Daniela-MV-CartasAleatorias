e5f5d13dd3c3656e1224a43e08c952b488717738
Daniela-MV <danmv.0991@gmail.com>
2025-04-03 23:35:16 +0000

    Ya casi

--- src/app.js
+++ src/app.js
@@ -10,11 +10,23 @@ window.onload = function() {
   let numbers= ["A","2","3","4","5","6","7","8","9","10","K","Q","J"];
   let figures= [ "♦","♥","♠","♣"];
   
+  let container= document.getElementById("card");
+  let figuraSup= document.getElementById("figureUp");
+  let numero= document.getElementById("number");
+  let figuraInf= document.getElementById("figureButtom");
+  let boton= document.getElementById("btn");
+
   function showCard(){
   let onlyNumber= numbers[Math.floor(Math.random() * numbers.length)];
   let onlyFigures= figures[Math.floor(Math.random() * figures.length)];
-  let showMyCard=  `${onlyNumber} ${onlyFigures}`;
-  // document.getElementById("cards").style.background = "red";
-  // document.getElementById("cards").innerHTML = showMyCard; 
+
+  figuraSup.textContent = onlyFigures;
+  numero.textContent = onlyNumber;
+  figuraInf.textContent = onlyFigures;
+  container.style.color= (onlyFigures === "♦" || onlyFigures === "♥")? "red" : "black";
   }
 
+  boton.addEventListener("click", showCard);
+
+ showCard();
+}
