7e15789afd4ccaf1561bdac5b29c267e5be4ef9f
Daniela-MV <danmv.0991@gmail.com>
2025-04-03 19:49:56 +0000

    HELP

--- src/app.js
+++ src/app.js
@@ -7,11 +7,14 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  let numbers= [2,3,4,5,6,7,8,9,10,K,Q,J,A];
-  let figuras= [ ♦,♥,♠,♣];
-  for(let i= 0; i < numbers.length; i++){
-    for (let j= 0; j < figuras.length; j++){
-      return numbers[i] + figuras[j]
-    }
+  let numbers= ["A","2","3","4","5","6","7","8","9","10","K","Q","J"];
+  let figures= [ "♦","♥","♠","♣"];
+  
+  function showCard(){
+  let onlyNumber= numbers[Math.floor(Math.random() * numbers.length)];
+  let onlyFigures= figures[Math.floor(Math.random() * figures.length)];
+  let showMyCard=  `${onlyNumber} ${onlyFigures}`;
+  // document.getElementById("cards").style.background = "red";
+  // document.getElementById("cards").innerHTML = showMyCard; 
   }
-};
+
